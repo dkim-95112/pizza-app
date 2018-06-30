07501187cbafc66fd642db1fc89a55275fcfb2e1
dkim-95112 <innerb@gmail.com>
2018-06-30 01:14:29 -0500

    Adding css-loader. Importing css

--- index.jsx
+++ index.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import JumboTron from './src/jumbotron/index.jsx'
+import './index.css'
 
-class PizzaApp extends React.Component {
-    render() {
-        debugger
-        switch (location.hash){
-            default:
-            return (
-                <div>
-                    <JumboTron/>
-                    <div>foo</div>
-                </div>
-            )
-        }
-    }
+function Franchise(props){
+  return (
+    <div className="franchise">{props.name}</div>
+  )
 }
 
+class PizzaApp extends React.Component {
+  render() {
+    debugger
+    switch (location.hash) {
+      default:
+        return (
+          <div>
+            <JumboTron/>
+            <Franchise name="new york"/>
+            <Franchise name="chicago"/>
+            <Franchise name="san francisco"/>
+          </div>
+        )
+    }
+  }
+}
+
+
 ReactDOM.render(
-    <PizzaApp />,
-    document.getElementById('root')
+  <PizzaApp/>,
+  document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 module.exports = {
-    mode: 'development',
-    entry: './index.jsx',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
-    }
+  mode: 'development',
+  entry: './index.jsx',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
