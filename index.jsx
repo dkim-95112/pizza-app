9243a6a4ebf9c0aad44fb7d6f78a35f42530155e
dkim-95112 <innerb@gmail.com>
2018-06-30 11:58:13 -0500

    Selecting location key

--- index.jsx
+++ index.jsx
@@ -3,26 +3,44 @@ import ReactDOM from 'react-dom'
 import JumboTron from './src/jumbotron/index.jsx'
 import './index.css'
 
-function Franchise(props){
-  return (
-    <div className="franchise">{props.name}</div>
-  )
+class Franchise extends React.Component {
+  render() {
+    return (
+      <div className="franchise">
+        {this.props.locationKey}
+        <a href="#" onClick={e => this.props.cbSelect(e, this.props.locationKey)}>
+          Order Pizza Online
+        </a>
+      </div>
+    )
+  }
 }
 
 class PizzaApp extends React.Component {
-  render() {
-    debugger
-    switch (location.hash) {
-      default:
-        return (
-          <div>
-            <JumboTron/>
-            <Franchise name="new york"/>
-            <Franchise name="chicago"/>
-            <Franchise name="san francisco"/>
-          </div>
-        )
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedLocationKey: null
     }
+    this.cbSelect = this.cbSelect.bind(this)
+  }
+
+  cbSelect(e, locationKey) {
+    e.preventDefault();
+    this.setState({
+      selectedLocationKey: locationKey
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <JumboTron/>
+        <Franchise cbSelect={this.cbSelect} locationKey="new york"/>
+        <Franchise cbSelect={this.cbSelect} locationKey="chicago"/>
+        <Franchise cbSelect={this.cbSelect} locationKey="san francisco"/>
+      </div>
+    )
   }
 }
 
